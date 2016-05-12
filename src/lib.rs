714df5de5c36242e0745e09c85b941b5e3d31187
orhanbalci <orhanbalci@gmail.com>
2016-05-12 12:53:40 +0300

    steering accelerator calculator trait added to root module

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub use self::steerable::Steerable;
 pub use self::steering_behavior::SteeringBehavior;
 pub use self::limiter::Limiter;
 pub use self::steering_acceleration::SteeringAcceleration;
+pub use self::steering_acceleration::SteeringAccelerationCalculator;
 
 mod steerable;
 mod steering_behavior;
