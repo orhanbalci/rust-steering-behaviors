684831c023e2b9228e72de46db484719d3b5bd18
orhanbalci <orhanbalci@gmail.com>
2016-05-12 12:44:54 +0300

    steering acceleration calculator trait added

--- src/steering_acceleration.rs
+++ src/steering_acceleration.rs
@@ -5,7 +5,7 @@ use std::ops::MulAssign;
 
 /// Represents result of a steering behaviour computation. User can aggregate
 /// more than one behaviour result into single acceleration struct.
-pub struct SteeringAcceleration<T: BaseFloat + AddAssign + MulAssign + Copy + ApproxEq<T>> {
+pub struct SteeringAcceleration<T: BaseFloat + ApproxEq<T>> {
     /// linear acceleration component
     pub linear: Vector3<T>,
     /// angular acceleration component
@@ -13,7 +13,7 @@ pub struct SteeringAcceleration<T: BaseFloat + AddAssign + MulAssign + Copy + Ap
 }
 
 
-impl<T: BaseFloat + AddAssign + MulAssign + Copy + ApproxEq<T>> SteeringAcceleration<T> {
+impl<T: BaseFloat + ApproxEq<T>> SteeringAcceleration<T> {
     /// Creates a steering acceleration struct using given linear and angular components
     fn new(linear_acceleration: Vector3<T>, angular_acceleration: T) -> SteeringAcceleration<T> {
         SteeringAcceleration {
@@ -28,9 +28,10 @@ impl<T: BaseFloat + AddAssign + MulAssign + Copy + ApproxEq<T>> SteeringAccelera
     }
 
     /// Sets both compononents to zero
-    fn set_zero(self: &mut Self) {
+    fn set_zero(self: &mut Self) -> &mut Self {
         self.angular = T::zero();
         self.linear = Vector3::zero();
+        self
     }
 
     /// 
@@ -48,20 +49,36 @@ impl<T: BaseFloat + AddAssign + MulAssign + Copy + ApproxEq<T>> SteeringAccelera
     }
 
     ///
-    fn mulAdd(self : &mut Self, other : SteeringAcceleration<T>, scale : T)-> &mut Self{
+    fn mulAdd(self: &mut Self, other: SteeringAcceleration<T>, scale: T) -> &mut Self {
         self.angular = self.angular + (other.angular * scale);
         self.linear += other.linear * Vector3::repeat(scale);
         self
     }
-    
+
     ///
-    fn calculate_square_magnitude(self :  &Self)->T{
-        self.linear.as_point().distance_squared(&Vector3::zero().to_point()) + self.angular * self.angular
+    fn calculate_square_magnitude(self: &Self) -> T {
+        self.linear.as_point().distance_squared(&Vector3::zero().to_point()) +
+        self.angular * self.angular
     }
 
     ///
-    fn calculate_magnitude(self : &Self)->T {
+    fn calculate_magnitude(self: &Self) -> T {
         self.calculate_square_magnitude().sqrt()
     }
+}
 
+pub trait SteeringAccelerationCalculator<T: BaseFloat + ApproxEq<T>> {
+    fn calculate_steering<'a>(self: &mut Self,
+                              steering_acceleration: &'a mut SteeringAcceleration<T>)
+                              -> &'a mut SteeringAcceleration<T> {
+        if self.is_enabled() {
+            self.calculate_real_steering(steering_acceleration)
+        } else {
+            steering_acceleration.set_zero()
+        }
+    }
+    fn calculate_real_steering<'a>(self: &mut Self,
+                                   steering_acceleration: &'a mut SteeringAcceleration<T>)
+                                   -> &'a mut SteeringAcceleration<T>;
+    fn is_enabled(self: &Self) -> bool;
 }
